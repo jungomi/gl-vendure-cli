89b55273b9196e98a25d6e3fa0e59abe1ef9ded8
Michael Jungo <michaeljungo92@gmail.com>
2026-05-05 22:08:05 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Very Small CLI using Vendure
+
+This is a very small CLI in Java using the e-commerce framework [Vendure][vendure] as a service. This serves as an example for the software engineering
+lecture (_Génie logiciel_) 2026.
+
+In order to use this, you must have [Vendure][vendure] installed and running
+(see [Vendure Documentation - Installation][vendure-install]).
+
+[vendure]: https://github.com/vendurehq/vendure
+[vendure-install]: https://docs.vendure.io/current/core/getting-started/installation
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<JavaCompile> {
+    val compilerArgs = options.compilerArgs
+    compilerArgs.add("-Aproject=${project.group}/${project.name}")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

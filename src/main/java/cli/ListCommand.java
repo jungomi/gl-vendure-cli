7dd31b94a0a3fd02483d9b9e3563f589c7f21862
Michael Jungo <michaeljungo92@gmail.com>
2026-05-06 10:49:39 +0200

    feat(cli): provide a `--format` option for the list command

--- src/main/java/cli/ListCommand.java
+++ src/main/java/cli/ListCommand.java
@@ -1,21 +1,40 @@
 package cli;
 
+import format.*;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.ParentCommand;
+import product.ProductList;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "list", description = "List the available products")
-public class ListCommand  implements Callable<Integer> {
+public class ListCommand implements Callable<Integer> {
     // The parent command to access the "global" options,
     // i.e. options that are shared across all subcommands.
     @ParentCommand
     private Cli parentCommand;
 
+    @Option(names = {"-f", "--format"},
+            description = "Format of the output, one of ${COMPLETION-CANDIDATES} [Default: ${DEFAULT-VALUE}]")
+    private Format format = Format.table;
+
     @Override
     public Integer call() throws Exception {
-        IO.println(String.format("list -- URL: %s", parentCommand.getUrl()));
-        // TODO: logic of listing the products
+        String url = parentCommand.getUrl();
+        IO.println(String.format("list -- URL: %s", url));
+
+        ProductList productList = ProductList.retrieveProducts(url);
+        FormatStrategy formatStrategy = getFormatStrategy();
+        IO.println(formatStrategy.formatProductList(productList));
+
         return 0;
+    }
+
+    private FormatStrategy getFormatStrategy() {
+        return switch (format) {
+            case table -> new TableFormat();
+            case json -> new JsonFormat();
+        };
     }
 }
